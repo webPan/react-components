a67a4aacebe309ef904e0348cb2b71eaad9e324f
qing_321 <qing_321@126.com>
2024-10-17 18:38:27 +0800

    feat: 文档生成

--- .dumirc.ts
+++ .dumirc.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   outputPath: 'docs',
+  base: '/react-components/', // 基础路径
+  publicPath: '/react-components/', // 静态资源的公共路径
   themeConfig: {
     name: 'react',
     nav: [{ title: '组件库', link: '/components' }],
     docDirs: 'md',
-    base: '/react-components',
   },
 });
