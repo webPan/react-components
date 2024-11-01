853c211fd00c7c8c86051298f7dc0beefd9d4412
qing_321 <qing_321@126.com>
2024-11-01 18:14:35 +0800

    feat: 删除源代码

--- src/QkDrawerForm/index.tsx
+++
@@ -1,9 +0,0 @@
-import { DrawerForm, type DrawerFormProps } from '@ant-design/pro-components';
-import React from 'react';
-
-type QkDrawerFormType = DrawerFormProps;
-const QkDrawerForm: React.FC<QkDrawerFormType> = (props) => {
-  return <DrawerForm {...props} />;
-};
-
-export default QkDrawerForm;
--- src/QkModalForm/index.tsx
+++
@@ -1,9 +0,0 @@
-import { ModalForm, type ModalFormProps } from '@ant-design/pro-components';
-import React from 'react';
-
-type QkModalFormType = ModalFormProps;
-const QkModalForm: React.FC<QkModalFormType> = (props) => {
-  return <ModalForm {...props} />;
-};
-
-export default QkModalForm;
